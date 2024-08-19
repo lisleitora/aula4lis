8047876de11f471ecf0d4477ec12a21aee317199
lisLeitora <62620405+lisleitora@users.noreply.github.com>
2024-08-19 13:37:23 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,20 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    int[] ranNums = {29,238,4,7,82,938,2,3,0,1};
+    foreach(int num in ranNums){
+      if(num==2){
+         Console.WriteLine ("numero encontrado");
+      }
+      
+    }
+
+    
+        
+    
+    
+   
+    
+  }
+}
